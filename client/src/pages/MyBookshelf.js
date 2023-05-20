506ca313bb2da4c3aba616f13d8e73d516ef42a9
WilliamsE2 <erwilliams331@gmail.com>
2023-05-20 00:55:48 -0500

    Change list functionality

--- client/src/pages/MyBookshelf.js
+++ client/src/pages/MyBookshelf.js
@@ -7,16 +7,38 @@ import '../styles/MyBookshelf.css';
 import BookshelfCard from '../components/BookshelfCard.js';
 
 import listData from '../lists-data.json';
+import { Link } from 'react-router-dom';
 
 const MyBookshelf = () => {
+
+    const [listTitle, changeListTitle] = useState('Read');
+
+    const lists = [
+        'Read',
+        'My Reading List',
+        'Favorites'
+    ];
+
+    const handleListChange = (value) => {
+        changeListTitle(value);
+    };
 
     return (
         <div className='bookshelf-container'>
             <div className='bookshelf-column-left'>
-                <p>Left</p>
+                <div className='bookshelf-my-lists'>
+                    <p className='bookshelf-my-lists-text'>My Bookshelves</p>
+                </div>
+                <div className='bookshelf-my-lists-list'>
+                    {
+                        lists.map((list) => (
+                            <p className='bookshelf-my-lists-list-text' onClick={() => handleListChange(list)}>{list}</p>
+                        ))
+                    }
+                </div>
             </div>
             <div className='bookshelf-column-right'>
-                <p className='bookshelf-list-title'>List Title</p>
+                <p className='bookshelf-list-title'>{listTitle}</p>
                 <div className='bookshelf-list'>
                     {
                         listData.filter(book => {
