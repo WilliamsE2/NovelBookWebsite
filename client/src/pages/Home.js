5a146e8658a7028d74fd04c2cd64e911f513f270
WilliamsE2 <erwilliams331@gmail.com>
2023-05-22 23:43:09 -0500

    Start home page, banners

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,84 @@
 import '../styles/Home.css';
 
+// Components
+import BookSelector from '../components/BookSelector.js';
+
 const Home = () => {
+
+    const bookRecs = [
+        {
+            'title': 'Harry Potter and the Deathly Hallows',
+            'author': 'J.K. Rowling'
+        },
+        {
+            'title': 'Harry Potter and the Deathly Hallows',
+            'author': 'J.K. Rowling'
+        },
+        {
+            'title': 'Harry Potter and the Deathly Hallows',
+            'author': 'J.K. Rowling'
+        },
+        {
+            'title': 'Harry Potter and the Deathly Hallows',
+            'author': 'J.K. Rowling'
+        }
+    ];
+
     return (
         <div className='home-container'>
-            <div className='home'>
-                <div className='popular'>
-                    <h1>Home</h1>
+            <div className='home-section'>
+                <p className='home-section-title'>2023 Reading Challenge</p>
+                <div className='home-section-banner'>
+                    <img className='home-section-banner-image' src={require('../assets/frodo-banner.jpeg')} alt='Reading Banner'/>
                 </div>
+                <div className='home-section-recs'>
+                    {
+                        bookRecs.map(rec => (
+                            <BookSelector title={rec.title} author={rec.author} />
+                        ))
+                    }
+                </div>
+            </div>
 
-                <div className='genres'>
-                    <h1>Genres</h1>
+            <div className='home-section'>
+                <p className='home-section-title'>Popular</p>
+                <div className='home-section-banner'>
+                    <img className='home-section-banner-image' src={require('../assets/catcher-banner.jpeg')} alt='Reading Banner'/>
+                </div>
+                <div className='home-section-recs'>
+                    {
+                        bookRecs.map(rec => (
+                            <BookSelector title={rec.title} author={rec.author} />
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className='home-section'>
+                <p className='home-section-title'>Recommended</p>
+                <div className='home-section-banner'>
+                    <img className='home-section-banner-image' src={require('../assets/murakami-banner.png')} alt='Reading Banner'/>
+                </div>
+                <div className='home-section-recs'>
+                    {
+                        bookRecs.map(rec => (
+                            <BookSelector title={rec.title} author={rec.author} />
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className='home-section'>
+                <p className='home-section-title'>Can't go wrong with the classics...</p>
+                <div className='home-section-banner'>
+                    <img className='home-section-banner-image' src={require('../assets/gatsby-banner.jpeg')} alt='Reading Banner'/>
+                </div>
+                <div className='home-section-recs'>
+                    {
+                        bookRecs.map(rec => (
+                            <BookSelector title={rec.title} author={rec.author} />
+                        ))
+                    }
                 </div>
             </div>
         </div>
