87b78b7a8b4b606b389ac1d4dfb7bf3c713b697c
WilliamsE2 <erwilliams331@gmail.com>
2023-07-04 22:53:17 -0500

    Fixed grabbing list data and books, complete

--- client/src/pages/MyBookshelf.js
+++ client/src/pages/MyBookshelf.js
@@ -15,16 +15,15 @@ const MyBookshelf = () => {
 
     const [listData, changeListData] = useState([]);
     const [bookData, changeBookData] = useState([]);
+    const [bookSlice, changeBookSlice] = useState([]);
 
     const pageItemCount = 10;
     const [pageCount, changePageCount] = useState(0);
     const [currentPage, changeCurrentPage] = useState(1);
 
-    const [currentList, changeCurrentList] = useState('Read');
+    const [currentList, changeCurrentList] = useState(0);
     const [openCreateList, changeOpenCreateList] = useState(false);
     const [openDeleteList, changeOpenDeleteList] = useState(false);
-
-    const [bookSlice, changeBookSlice] = useState([{}]);
 
     useEffect(() => {
         fetch('http://localhost:3001/lists', {
@@ -42,48 +41,21 @@ const MyBookshelf = () => {
             }
         })
         .then(data => {
-            console.log(data);
             const jsonData = JSON.parse(data);
-            console.log(jsonData);
             changeListData(jsonData);
-            /*for (let i = 0; i < jsonData.length; i++) {
-                if (jsonData[i].list_name === 'Read') {
-                    return getListBooks(jsonData, jsonData[i].row_number, jsonData[i].list_name);
+
+            for (let i = 0; i < jsonData.length; i++) {
+                if (jsonData[i].list_name === 'Read' && jsonData[i].deletable === false) {
+                    changeCurrentList(i);
+                    changeBookData(jsonData[i].book_list);
+                    
+                    changePageCount(Math.ceil(jsonData[i].book_list.length / pageItemCount));
+                    changeCurrentPage(1);
+                    changeBookSlice(jsonData[i].book_list.slice(0, pageItemCount));
                 }
-            }*/
+            }
         });
     }, [userId]);
-
-    const getListBooks = (data, listNum, listName) => {
-        const bookIds = JSON.stringify(data[listNum - 1].list);
-
-        fetch('http://localhost:3001/lists/books', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({userId, bookIds}),
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            } else {
-                return response.text();
-            }
-        })
-        .then(data => {
-            const jsonData = JSON.parse(data);
-            console.log(jsonData);
-            changeBookData(jsonData);
-            setPagination(jsonData.length);
-            changeCurrentList(listName);
-        });
-    };
-
-    const setPagination = (bookAmount) => {
-        changePageCount(Math.ceil(bookAmount / pageItemCount));
-        handlePageChange(1);
-    };
 
     const handlePageChange = (i) => {
         changeCurrentPage(i);
@@ -91,9 +63,14 @@ const MyBookshelf = () => {
         changeBookSlice(bookData.slice(startItem - 1, (pageItemCount * i)));
     };
 
-    const handleListChange = (listNum, listName) => {
-        const data = listData;
-        //getListBooks(data, listNum, listName);
+    const handleListChange = (num) => {
+        changeCurrentList(num);
+
+        const list = listData[num];
+        changeBookData(list.book_list);
+        changePageCount(Math.ceil(list.book_list.length / pageItemCount));
+        changeCurrentPage(1);
+        changeBookSlice(list.book_list.slice(0, pageItemCount));
     };
 
     return (
@@ -108,7 +85,7 @@ const MyBookshelf = () => {
                             <LoadingSpinner />
                         : 
                             listData.map((list) => (
-                                <p className='bookshelf-my-lists-list-text' onClick={() => handleListChange(list.row_number, list.list_name)}>{list.list_name}</p>
+                                <p className='bookshelf-my-lists-list-text' onClick={() => handleListChange(list.row_number - 1, list.list_name)}>{list.list_name}</p>
                             ))
                     }
                 </div>
@@ -126,63 +103,72 @@ const MyBookshelf = () => {
                 }
             </div>
             <div className='bookshelf-column-right'>
-                <div className='bookshelf-column-right-top'>
-                    <p className='bookshelf-list-title'>{currentList}</p>
-                    {
-                        openDeleteList ? 
-                            <div className='bookshelf-delete-list'>
-                                <p className='bookshelf-delete-list-text'>Delete this list?</p>
-                                <img className='bookshelf-create-list-image bookshelf-create-list-image-rotate' onClick={() => changeOpenDeleteList(!openDeleteList)} src={require('../assets/plus-icon.png')} alt='Plus Sign'/>
-                                <img className='bookshelf-create-list-image' onClick={() => changeOpenDeleteList(!openDeleteList)} src={require('../assets/checkmark-icon.png')} alt='Checkmark'/>
-                            </div> 
-                        : 
-                            <div className='bookshelf-delete-list-button' onClick={() => changeOpenDeleteList(!openDeleteList)}>
-                                <img className='bookshelf-delete-list-image' src={require('../assets/delete-icon.png')} alt='Delete'/>
-                            </div>
-                    }
-                </div>
                 {
-                    bookData.length < 1 ? '' : 
-                        <>
-                        <Pagination 
-                            className='bookshelf-list-pagination' 
-                            dir='ltr'
-                            page={currentPage} 
-                            count={pageCount} 
-                            onChange={(event, value) => handlePageChange(value)} 
-                            variant='outlined' 
-                            shape='rounded' 
-                            showFirstButton 
-                            showLastButton 
-                        />
-                        <div className='bookshelf-list'>
-                            {
-                                bookSlice.filter(book => {
-                                    return book;
-                                }).map((book, index) => (
-                                    <BookshelfCard 
-                                        bookId={book.bookId}
-                                        title={book.title}
-                                        author={book.author}
-                                        rating={book.user_rating}
-                                        isCommunity={false}
-                                        addedDate={book.added_date}
-                                    />
-                                ))
-                            }
-                        </div>
-                        <Pagination 
-                            className='bookshelf-list-pagination' 
-                            dir='ltr'
-                            page={currentPage} 
-                            count={pageCount} 
-                            onChange={(event, value) => handlePageChange(value)} 
-                            variant='outlined' 
-                            shape='rounded' 
-                            showFirstButton 
-                            showLastButton 
-                        />
-                        </>
+                    listData.length < 1 ? 
+                            <LoadingSpinner /> 
+                        : 
+                            <div className='bookshelf-column-right-top'>
+                                <p className='bookshelf-list-title'>{listData[currentList].list_name}</p>
+                                {
+                                    listData[currentList].deletable ? 
+                                        openDeleteList ? 
+                                            <div className='bookshelf-delete-list'>
+                                                <p className='bookshelf-delete-list-text'>Delete this list?</p>
+                                                <img className='bookshelf-create-list-image bookshelf-create-list-image-rotate' onClick={() => changeOpenDeleteList(!openDeleteList)} src={require('../assets/plus-icon.png')} alt='Plus Sign'/>
+                                                <img className='bookshelf-create-list-image' onClick={() => changeOpenDeleteList(!openDeleteList)} src={require('../assets/checkmark-icon.png')} alt='Checkmark'/>
+                                            </div> 
+                                        : 
+                                            <div className='bookshelf-delete-list-button' onClick={() => changeOpenDeleteList(!openDeleteList)}>
+                                                <img className='bookshelf-delete-list-image' src={require('../assets/delete-icon.png')} alt='Delete'/>
+                                            </div>
+                                    : ''
+                                }
+                            </div>
+                }
+                {
+                    bookSlice.length < 1 ? 
+                            <LoadingSpinner /> 
+                        : 
+                            <>
+                            <Pagination 
+                                className='bookshelf-list-pagination' 
+                                dir='ltr'
+                                page={currentPage} 
+                                count={pageCount} 
+                                onChange={(event, value) => handlePageChange(value)} 
+                                variant='outlined' 
+                                shape='rounded' 
+                                showFirstButton 
+                                showLastButton 
+                            />
+                            <div className='bookshelf-list'>
+                                {
+                                    bookSlice.filter(book => {
+                                        return book;
+                                    }).map((book, index) => (
+                                        <BookshelfCard 
+                                            bookId={book.book_id}
+                                            title={book.book_title}
+                                            author={book.author_name}
+                                            rating={0}
+                                            isCommunity={false}
+                                            addedDate={'Dummy Date'}
+                                        />
+                                    ))
+                                }
+                            </div>
+                            <Pagination 
+                                className='bookshelf-list-pagination' 
+                                dir='ltr'
+                                page={currentPage} 
+                                count={pageCount} 
+                                onChange={(event, value) => handlePageChange(value)} 
+                                variant='outlined' 
+                                shape='rounded' 
+                                showFirstButton 
+                                showLastButton 
+                            />
+                            </>
                 }
             </div>
         </div>
