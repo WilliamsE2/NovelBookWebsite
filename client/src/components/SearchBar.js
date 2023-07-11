35139ca184a6e26c1344bc1c2d496ed2d4074251
WilliamsE2 <erwilliams331@gmail.com>
2023-07-10 22:20:58 -0500

    Search bar gets all books, links to book page

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,31 +1,30 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 
 import '../styles/SearchBar.css';
 
 const SearchBar = () => {
 
-    const [searchInput, setSearchInput] = useState("");
+    const [bookData, changeBookData] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:3001/all-books')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            } else {
+                return response.text();
+            }
+        })
+        .then(data => {
+            changeBookData(JSON.parse(data));
+        });
+    }, []);
 
     const handleCollapse = () => {
         setSearchInput('');
     };
-
-    const countries = [
-        { title: "Basic Economics", author: "Thomas Sowell" },
-        { title: "You Can't Be Serious", author: "Kal Penn" },
-        { title: "Divergent", author: "Veronica Roth" },
-        { title: "1776", author: "David McCullough" },
-        { title: "Atomic Habits", author: "James Clear" },
-        { title: "Shoe Dog", author: "Phil Knight" },
-        { title: "The Girl with the Dragon Tattoo", author: "Stieg Larsson" },
-        { title: "Dune", author: "Frank Herbert" },
-        { title: "American Psycho", author: "Bret Easton Ellis" },
-        { title: "Casino Royale", author: "Ian Fleming" },
-        { title: "Game of Thrones", author: "George R.R. Martin" },
-        { title: "The Hobbit", author: "J.R.R. Tolkien" },
-        { title: "This is an increidbly long title just trying to do a big long test ahhaahhah", author: "Author" }
-    ];
 
     return (
         <div className='search-div'>
@@ -35,21 +34,21 @@ const SearchBar = () => {
                 <div className='search-results-box-outer'>
                     <div className='search-results-box-inner'>
                     {
-                        countries.filter(post => {
+                        bookData.filter(book => {
                             if (searchInput === '') {
                                 return false;
-                            } else if (post.title.toLocaleLowerCase().includes(searchInput.toLowerCase())) {
-                                return post;
-                            } else if (post.author.toLocaleLowerCase().includes(searchInput.toLowerCase())) {
-                                return post;
+                            } else if (book.book_title.toLocaleLowerCase().includes(searchInput.toLowerCase())) {
+                                return book;
+                            } else if (book.author_name.toLocaleLowerCase().includes(searchInput.toLowerCase())) {
+                                return book;
                             } else {
                                 return false;
                             }
-                        }).map((post, index) => (
-                            <div className='search-result' key={index}>    
-                                <p className='search-result-text search-result-title'>{post.title}</p>
-                                <p className='search-result-text search-result-author'>by {post.author}</p>
-                            </div>
+                        }).map((book, index) => (
+                            <Link to={`/layout/book/${book.book_id}`} key={index} className="search-result">
+                                <p className='search-result-text search-result-title'>{book.book_title}</p>
+                                <p className='search-result-text search-result-author'>by {book.author_name}</p>
+                            </Link>
                         ))
                     }
                     </div>
