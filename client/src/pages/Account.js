c2a2b070dbb9aa1ac8ce668e19f7173c323c4d79
WilliamsE2 <erwilliams331@gmail.com>
2023-07-11 09:19:54 -0500

    Account join date formatted

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -14,6 +14,8 @@ const Account = () => {
     const [accountData, changeAccountData] = useState([]);
     const [reviewData, changeReviewData] = useState([]);
 
+    const [joinDate, changeJoinDate] = useState('');
+
     useEffect(() => {
         fetch('http://localhost:3001/account', {
             method: 'POST',
@@ -31,6 +33,7 @@ const Account = () => {
         })
         .then(data => {
             changeAccountData(JSON.parse(data));
+            changeJoinDate(new Date(JSON.parse(data).creation_date).toLocaleDateString(undefined, { year: "numeric", month: "long", day: "numeric" }));
         }); 
     }, [userId]);
 
@@ -50,38 +53,42 @@ const Account = () => {
             }
         })
         .then(data => {
-            console.log(JSON.parse(data));
             changeReviewData(JSON.parse(data));
         }); 
     }, [userId]);
 
     return (
         <div className='account-container'>
-            <div className='account-columns'>
-                <div className='account-column-left'>
-                    {
-                        accountData.length < 1 ? 
-                            <LoadingSpinner />
-                        : 
+            {
+                accountData.length < 1 ? 
+                    <LoadingSpinner />
+                : 
+                    <div className='account-columns'>
+                        <div className='account-column-left'>
                             <img className='account-profile-image' src={require(`../assets/profile-pics/${accountData.profile_pic_id}.jpeg`)} alt='Profile'/>
-                    }
-                    <Link to="editaccount" className="account-edit-link">
-                        <div className='account-edit-button'>    
-                                <p className='account-edit-button-text'>Edit Profile</p>
+                            <Link to="editaccount" className="account-edit-link">
+                                <div className='account-edit-button'>    
+                                        <p className='account-edit-button-text'>Edit Profile</p>
+                                </div>
+                            </Link>
                         </div>
-                    </Link>
-                </div>
-                <div className='account-column-right'>
-                    <div className='account-name'>
-                        <p className='account-name-text'>{accountData.first_name} {accountData.last_name}</p>
-                    </div>
-                    <div className='account-info'>
-                        <p className='account-info-text'>Joined {accountData.creation_date}</p>
-                        <p className='account-info-text'>{accountData.books_read} books read</p>
-                        <p className='account-info-text'>{accountData.number_of_reviews} reviews written</p>
-                    </div>
-                </div>
-            </div>
+                        <div className='account-column-right'>
+                            <div className='account-name'>
+                                <p className='account-name-text'>{accountData.first_name} {accountData.last_name}</p>
+                            </div>
+                            <div className='account-info'>
+                                {
+                                    joinDate.length < 1 ? 
+                                            '' 
+                                        : 
+                                            <p className='account-info-text'>Joined {joinDate}</p>
+                                }
+                                <p className='account-info-text'>{accountData.books_read} books read</p>
+                                <p className='account-info-text'>{accountData.number_of_reviews} reviews written</p>
+                            </div>
+                        </div>
+                    </div>       
+            }
             <div className='account-activity'>
                 <p className='account-activity-text'>Activity</p>
                 <div className='account-review-list'>
